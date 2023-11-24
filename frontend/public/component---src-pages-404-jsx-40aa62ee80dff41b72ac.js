fe2ca4cd5bb082d81b76dc40e04e8438cfd9c283
A.M. <andrej2000aa@gmail.com>
2023-11-24 15:38:34 +0100

    ppp

---
+++ frontend/public/component---src-pages-404-jsx-40aa62ee80dff41b72ac.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[256],{7291:function(e,t,l){l.r(t),l.d(t,{default:function(){return r}});var n=l(7294);function r(){return n.createElement("main",null,n.createElement("div",{className:"tekst"},n.createElement("h1",null,"Error"),n.createElement("h3",null,"Ooops, Page not found")))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-40aa62ee80dff41b72ac.js.map
---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import '../assets/css/main.scss'
+
+
+
+
+export default function Error() {
+  return(
+      <main>
+        <div className="tekst">
+            <h1>Error</h1>
+            <h3>Ooops, Page not found</h3>
+        </div>
+
+
+      </main>
+  )
+}
